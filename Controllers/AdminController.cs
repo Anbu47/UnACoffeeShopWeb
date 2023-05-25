60e7edf9582169410d6906120c654733ed3e24af
Thai Ngoc Vo <thaingoc58kd@gmail.com>
2023-05-25 16:25:03 +0700

    feat: Admin login with Admin Model and Admin Controller

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnACoffeeShop.HelperScript;
+using UnACoffeeShop.Models.ShopItemModel;
+
+namespace UnACoffeeShop.Controllers
+{
+    [ApiController]
+    [Route("api/data")]
+    public class AdminController : ControllerBase
+
+    {
+        //No Respond
+        [HttpPost("login")]
+        public bool Login([FromBody] AdminModel adminModel)
+        {
+            if (adminModel.AccountID == "ThaiNgocxinhdep" && adminModel.Password == "ThaiNgociscute")
+            {
+                return true;
+            }
+                return false;
+        }
+    }
+}
---
+++ Models/AdminModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+namespace UnACoffeeShop.Models.ShopItemModel
+{
+    public class AdminModel
+    {
+        [JsonPropertyName("AccountID")] public string AccountID { get; set; }
+        [JsonPropertyName("Password")] public string Password { get; set; }
+    }
+}
