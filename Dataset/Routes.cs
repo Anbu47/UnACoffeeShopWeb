c41d80c4be55e3da87321517f1d5153f4adc6103
Thai Ngoc Vo <thaingoc58kd@gmail.com>
2023-06-04 17:18:49 +0700

    fix: routes paths

--- Dataset/Routes.cs
+++ Dataset/Routes.cs
@@ -2,9 +2,9 @@
 {
     public static class Routes
     {
-        public const string Item = "./Dataset/Item.json";
-        public const string Decorator = "./Dataset/Decorator.json";
-        public const string CartOrder = "./Dataset/CartOrder.json";
-        public const string Profile = "./Dataset/Profile.json";
+        public const string Item = "Dataset/Item.json";
+        public const string Decorator = "Dataset/Decorator.json";
+        public const string CartOrder = "Dataset/CartOrder.json";
+        public const string Profile = "Dataset/Profile.json";
     }
 }
